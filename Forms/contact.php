cc005e01bcacb6ba4aebe2d770da7867272f99e4
Nikolay Ivanov <nikolay_sashev@abv.bg>
2017-04-20 21:27:59 +0300

    Forms

---
+++ Forms/contact.php
@@ -0,0 +1,19 @@
+<?php require('./header.php'); ?>
+
+      <form method='POST'>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="checkbox">
+
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+      
+<?php require('./footer.php'); ?>
