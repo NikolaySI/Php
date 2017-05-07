6413d2602218a8a248fbef62c6d33a5ac2cde5af
Nikolay Ivanov <nikolay_sashev@abv.bg>
2017-05-07 14:32:59 +0300

    Add step one folder

--- Rendering HTML/about.php
+++
@@ -1,8 +0,0 @@
-<?php require('./header.php'); ?>
-
-      <div class="starter-template">
-        <h1>About starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-      
-<?php require('./footer.php'); ?>
